e19db0a78f468348bf9539c0df873c344520f7cd
Taurta <surta.nr@yandex.ru>
2021-12-08 18:17:38 +0300

    Deleted old files and added a video block

--- wp-content/themes/ys-wp-theme/src/_configs/webpack.base.conf.js
+++ wp-content/themes/ys-wp-theme/src/_configs/webpack.base.conf.js
@@ -1,9 +1,9 @@
-const path                 = require('path'),
-      MiniCssExtractPlugin = require('mini-css-extract-plugin'),
-      CopyWebpackPlugin    = require('copy-webpack-plugin'),
-      {CleanWebpackPlugin} = require('clean-webpack-plugin'),
-      {PATHS, getEntries}  = require('./helper'),
-      webpack              = require('webpack');
+const path = require('path'),
+    MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    CopyWebpackPlugin = require('copy-webpack-plugin'),
+    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    { PATHS, getEntries } = require('./helper'),
+    webpack = require('webpack');
 module.exports = {
     optimization: {
         minimize: true,
@@ -16,73 +16,80 @@ module.exports = {
         hints: false
     },
     // Убирает логирование плагинов в терминал (чтобы проще читалось)
-    stats    : {children: false},
-    resolve  : {
+    stats: { children: false },
+    resolve: {
         alias: {
-            '@theme'          : PATHS.theme,
-            '@assets'         : PATHS.assets,
-            '@base-assets'    : PATHS.baseAssets,
-            '@theme-assets'   : PATHS.themeAssets,
+            '@theme': PATHS.theme,
+            '@assets': PATHS.assets,
+            '@base-assets': PATHS.baseAssets,
+            '@theme-assets': PATHS.themeAssets,
             '@homepage-assets': PATHS.homepageAssets,
-            '@plugins'        : PATHS.plugins,
-            '@exjs'           : path.join(PATHS.assets, '/libs/extendedjs'),
+            '@plugins': PATHS.plugins,
+            '@exjs': path.join(PATHS.assets, '/libs/extendedjs'),
         }
     },
-    entry    : getEntries(),
-    output   : {
-        filename  : 'js/[name].min.js',
-        path      : PATHS.dist,
+    entry: getEntries(),
+    output: {
+        filename: 'js/[name].min.js',
+        path: PATHS.dist,
         publicPath: '/dist'
     },
-    module   : {
+    module: {
         //noParse: /jquery/,
         rules: [{
-            test   : /\.js$/,
-            loader : 'babel-loader',
+            test: /\.js$/,
+            loader: 'babel-loader',
             exclude: /node_modules/,
             options: {
                 presets: ['@babel/preset-env'],
                 plugins: ['@babel/plugin-proposal-class-properties', "transform-regenerator"]
             }
         }, {
-            test   : /\.(png|jpg|gif|svg)$/,
-            loader : 'file-loader',
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader',
             options: {
-                name      : '[name].[ext]',
+                name: '[name].[ext]',
                 outputPath: 'images'
             }
         }, {
-            test   : /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            loader : 'file-loader',
+            test: /\.mp4$/,
+            loader: 'file-loader',
             options: {
-                name      : '[name].[ext]',
+                name: '[name].[ext]',
+                outputPath: 'video'
+            }
+        }, {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            loader: 'file-loader',
+            options: {
+                name: '[name].[ext]',
                 outputPath: 'fonts'
             }
         }, {
             test: /\.s[ca]ss$/,
-            use : [
+            use: [
                 MiniCssExtractPlugin.loader,
                 {
-                    loader : 'css-loader',
-                    options: {importLoaders: 2} // 2 - sass-loader, postcss-loader
+                    loader: 'css-loader',
+                    options: { importLoaders: 2 } // 2 - sass-loader, postcss-loader
                 },
                 {
-                    loader : 'postcss-loader',
-                    options: {config: {path: `${PATHS.configs}/postcss/postcss.config.js`}}
+                    loader: 'postcss-loader',
+                    options: { config: { path: `${PATHS.configs}/postcss/postcss.config.js` } }
                 },
                 'sass-loader'
             ]
         }, {
             test: /\.css$/,
-            use : [
+            use: [
                 MiniCssExtractPlugin.loader,
                 {
-                    loader : 'css-loader',
-                    options: {importLoaders: 1} // 1 - sass-loader
+                    loader: 'css-loader',
+                    options: { importLoaders: 1 } // 1 - sass-loader
                 },
                 {
-                    loader : 'postcss-loader',
-                    options: {config: {path: `${PATHS.configs}/postcss/postcss.config.js`}}
+                    loader: 'postcss-loader',
+                    options: { config: { path: `${PATHS.configs}/postcss/postcss.config.js` } }
                 },
             ],
         }]
@@ -91,7 +98,7 @@ module.exports = {
         //     $: 'jquery',
         //     jquery: 'jQuery',
     },
-    plugins  : [
+    plugins: [
         new MiniCssExtractPlugin({
             filename: 'css/[name].css',
         }),
@@ -102,14 +109,15 @@ module.exports = {
         }),
 
         new CopyWebpackPlugin([
-            {from: `${PATHS.src}/assets/images`, to: `${PATHS.dist}/images`},
-            {from: `${PATHS.src}/assets/fonts`, to: `${PATHS.dist}/fonts`},
+            { from: `${PATHS.src}/assets/images`, to: `${PATHS.dist}/images` },
+            { from: `${PATHS.src}/assets/video`, to: `${PATHS.dist}/video` },
+            { from: `${PATHS.src}/assets/fonts`, to: `${PATHS.dist}/fonts` },
         ]),
 
         new webpack.ProvidePlugin({
-            "$"            : "jquery",
-            "jQuery"       : "jquery",
-            "window.$"     : "jquery",
+            "$": "jquery",
+            "jQuery": "jquery",
+            "window.$": "jquery",
             "window.jQuery": "jquery"
         }),
     ],
--- wp-content/themes/ys-wp-theme/src/entry/home.js
+++ wp-content/themes/ys-wp-theme/src/entry/home.js
@@ -1,9 +1,9 @@
 import "../assets/scss/pages/home.scss";
 
 import 'owl.carousel';
-import {swiperHomeReviews} from "../assets/js/components/ui-kit/swiper";
-import {carousel3d} from "../assets/js/components/home/carousel-3D";
+import { swiperHomeReviews } from "../assets/js/components/ui-kit/swiper";
+import "../assets/js/components/home/video";
 
 document.addEventListener("DOMContentLoaded", function () {
-    swiperHomeReviews().then(() => {});
+    swiperHomeReviews().then(() => { });
 });
