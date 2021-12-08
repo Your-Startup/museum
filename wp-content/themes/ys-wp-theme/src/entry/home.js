ee8b2884b17b0bb5fe2b44c6ea7b7cb29ade2081
Taurta <surta.nr@yandex.ru>
2021-12-08 20:59:38 +0300

    Added slider on home

--- wp-content/themes/ys-wp-theme/src/entry/home.js
+++ wp-content/themes/ys-wp-theme/src/entry/home.js
@@ -1,9 +1,6 @@
 import "../assets/scss/pages/home.scss";
 
 import 'owl.carousel';
-import { swiperHomeReviews } from "../assets/js/components/ui-kit/swiper";
 import "../assets/js/components/home/video";
+import "../assets/js/components/home/slider";
 
-document.addEventListener("DOMContentLoaded", function () {
-    swiperHomeReviews().then(() => { });
-});
